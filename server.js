81eadf5b64f64da19f9df37e903fd31c424a4a46
navjotsinghcheema <navjotsinghcheema@hotmail.com>
2016-09-23 18:07:00 +0530

    compatibility with heroku

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 9999;
+//accessing port from the environment variable
+//uppercase is a must for const keyword
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    console.log("The request header is != http");
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(port, function () {
+  console.log('Express server is up on port '+port);
 });
